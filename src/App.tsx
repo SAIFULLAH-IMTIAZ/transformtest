314f9ad319f086682a424861a688962d359e7993
SAIFULLAH-IMTIAZ <saifullahimtiaz@gmail.com>
2022-03-18 16:34:36 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import {useState} from "react";
+
 
 function App() {
+    const [image1, setImage1] = useState(true);
+    const [image2, setImage2] = useState(false);
+    const [image3, setImage3] = useState(false);
+    const [image4, setImage4] = useState(false);
+
+    const fnimage1 = () => {
+        setImage1(true)
+        setImage2(false)
+        setImage3(false)
+        setImage4(false)
+    }
+
+    const fnimage2 = () => {
+        setImage1(false)
+        setImage2(true)
+        setImage3(false)
+        setImage4(false)
+    }
+
+    const fnimage3 = () => {
+        setImage1(false)
+        setImage2(false)
+        setImage3(true)
+        setImage4(false)
+    }
+
+    const fnimage4 = () => {
+        setImage1(false)
+        setImage2(false)
+        setImage3(false)
+        setImage4(true)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Radio">
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">images</FormLabel>
+          <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="female"
+              name="radio-buttons-group"
+          >
+            <FormControlLabel value="image1" control={<Radio onClick={fnimage1}/>} label="image1" />
+            <FormControlLabel value="image2" control={<Radio onClick={fnimage2}/>} label="image2" />
+            <FormControlLabel value="image3" control={<Radio onClick={fnimage3}/>} label="image3" />
+            <FormControlLabel value="image4" control={<Radio onClick={fnimage4}/>} label="image4" />
+          </RadioGroup>
+        </FormControl>
+      </div>
+      <div className="App-header">
+          { image1 &&
+              <img className="image"  src={"../public/images/image1"}/>}
+          {image2 &&
+              <img className="image"  src={"../public/images/image2"}/>}
+          {image3 &&
+              <img className="image"  src={"../public/images/image3"}/>}
+          {image4 &&
+              <img className="image"  src={"../public/images/image4"}/>}
+      </div>
     </div>
   );
 }
